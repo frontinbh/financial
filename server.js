89d120529e089343b57158711da551fa213aae96
Zeno Rocha <zno.rocha@gmail.com>
2017-07-17 16:04:54 -0700

    Updates WeDeploy port

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.prepare()
   const server = express()
   server.use(bodyParser.urlencoded({ extended: false }))
   server.use(bodyParser.json())
-  
+
   server.post('/api/forecast/categories', require('./api/forecast/add-categories-revenues'))
   server.post('/api/forecast/categories-expenses', require('./api/forecast/add-categories-expenses'))
-  
+
   server.post('/api/forecast/expenses', require('./api/forecast/add-expenses'))
   server.put('/api/forecast/expenses/:id', require('./api/forecast/edit-expenses'))
   server.get('/api/forecast/expenses', require('./api/forecast/expenses'))
@@ -23,13 +23,13 @@ app.prepare()
   server.post('/api/forecast/revenue', require('./api/forecast/add-revenue'))
   server.put('/api/forecast/revenue/:id', require('./api/forecast/edit-revenue'))
   server.get('/api/forecast/revenue', require('./api/forecast/revenue'))
-  
+
   server.get('/api/dashboard', require('./api/dashboard'))
 
   server.get('*', (req, res) => handle(req, res))
 
-  server.listen(3000, (err) => {
+  server.listen(80, (err) => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log('> Ready on http://localhost:80')
   })
 })
